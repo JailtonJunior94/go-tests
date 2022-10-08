7e87fe1cc3e03c73e5527b2b31f5c0a0147f9f56
Jailton Junior <jailton.junior94@outlook.com>
2022-10-07 21:31:12 -0300

    implementando testes no usecase

--- internal/usecases/create_client_test.go
+++ internal/usecases/create_client_test.go
@@ -1,1 +1,38 @@
 package usecases
+
+import (
+	"testing"
+
+	"github.com/jailtonjunior94/go-tests/internal/entities"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type ClientRepositoryMock struct {
+	mock.Mock
+}
+
+func (c *ClientRepositoryMock) Save(client *entities.Client) error {
+	args := c.Called(client)
+	return args.Error(0)
+}
+
+func TestCreateClientUseCase_Execute(t *testing.T) {
+	mockRepo := new(ClientRepositoryMock)
+	mockRepo.On("Save", mock.Anything).Return(nil)
+
+	useCase := NewCreateClientUseCase(mockRepo)
+
+	input := &CreateClientInputDTO{
+		Name:  "Jailton Junior",
+		Email: "jailton.junior94@outlook.com",
+	}
+	output, err := useCase.Execute(input)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "Jailton Junior", output.Name)
+	assert.Equal(t, "jailton.junior94@outlook.com", output.Email)
+	assert.Equal(t, 0, output.Points)
+	mockRepo.AssertExpectations(t)
+	mockRepo.AssertNumberOfCalls(t, "Save", 1)
+}

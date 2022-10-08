dc5cabb1809b481cc12789d8c2a4cbf75ec0be2b
Jailton Junior <jailton.junior94@outlook.com>
2022-10-07 21:23:39 -0300

    implementando usecase

--- internal/usecases/create_client.go
+++ internal/usecases/create_client.go
@@ -1,5 +1,7 @@
 package usecases
 
+import "github.com/jailtonjunior94/go-tests/internal/entities"
+
 type CreateClientInputDTO struct {
 	Name  string
 	Email string
@@ -10,4 +12,34 @@ type CreateClientOutputDTO struct {
 	Name   string
 	Email  string
 	Points int
+}
+
+type CreateClientUseCase struct {
+	ClientRepository entities.ClientRepositoryInterface
+}
+
+func NewCreateClientUseCase(clientRepository entities.ClientRepositoryInterface) *CreateClientUseCase {
+	return &CreateClientUseCase{
+		ClientRepository: clientRepository,
+	}
+}
+
+func (c *CreateClientUseCase) Execute(input *CreateClientInputDTO) (*CreateClientOutputDTO, error) {
+	client, err := entities.NewClient(input.Name, input.Email)
+	if err != nil {
+		return nil, err
+	}
+
+	err = c.ClientRepository.Save(client)
+	if err != nil {
+		return nil, err
+	}
+
+	return &CreateClientOutputDTO{
+		ID:     client.ID,
+		Name:   client.Name,
+		Email:  client.Email,
+		Points: client.Points,
+	}, nil
+
 }
